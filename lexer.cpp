44dbd4a6168942cb770b07e348eb8517b80060a5
LalivT <59091636+LalivT@users.noreply.github.com>
2020-01-03 17:18:09 +0200

    Add files via upload

--- lexer.cpp
+++ lexer.cpp
@@ -24,21 +24,33 @@ vector<string> Lexer::Lex(const string fileName) {
     while(std::getline(objFile, line)) {
         string word = "";
         bool inExp = false;
+        bool inString = false;
         for (auto x : line)
         {
-            if (inExp == false && (x == ' ' | x == '(' | x == ')' | x == ',' | x == '\t' | x == '='))
+            if (inExp == false && inString == false && (x == ' ' | x == '(' | x == ')' | x == ',' | x == '\t' | x == '='))
             {
                 if(x == '=') {
                     inExp = true;
+                    word = word + x;
                 }
 
                 if(word != "" && word != "sim") {
                     words.push_back(word);
+                  //  inExp = false;
+                    //inString = false;
                 }
                 word = "";
+
             }
             else
             {
+                if(x == '\"') {
+                    if(inString == false) {
+                        inString = true;
+                    } else {
+                        inString = false;
+                    }
+                }
                 if(inExp == true) {
                     if(x != ' ') {
                         word = word + x;
